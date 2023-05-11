26e6737022e2e5c5db2da8cd8a3078ad325a3b8e
ppena77 <ppena77@alumno.uned.es>
2023-05-11 12:49:51 +0200

    SERVER.JS working v1.01

--- server.js
+++ server.js
@@ -24,44 +24,69 @@ let temporalJson = [];
 // Para cargar datos del archivo lecturas en nuestro objeto JSON temporal
 const loadJsonFileData = () => {
     return new Promise( (resolve, reject) => {
-        const fileRawData = fs.readFileSync('lecturas.json');
-        const fileData = JSON.parse(fileRawData);
-        temporalJson = fileData;
-        console.log('Temporal JSON with File Info only: ', temporalJson);
-        resolve();
+        try {
+            const fileRawData = fs.readFileSync('lecturas.json');
+            const fileData = JSON.parse(fileRawData);
+            temporalJson = fileData;
+            console.log('Json file data loaded in array');
+            resolve();
+        } catch (error) {
+            reject(error);
+        }        
     })
 };
 
 // Para añadir datos JSON mandados al servidor en nuestro objeto JSON temporal
 const addSentData = (data) => {
     return new Promise( (resolve, reject) => {
-        temporalJson.push(data);
-        console.log('Temporal JSON with appended info: ', temporalJson);
-        resolve();
+        try {
+            temporalJson.push(data);
+            console.log('Recieved Json data added to array');
+            resolve();
+        } catch (error) {
+            reject(error);
+        }        
     })
 };
 
 // Para registrar los datos del objeto JSON temporal en el archivo lecturas
 const registerJsonToFile = () => {
     return new Promise( (resolve, reject) => {
-        const jsonText = JSON.stringify(temporalJson);
-        fs.writeFileSync('lecturas.json', jsonText);
-        resolve();
+        try {
+            const jsonText = JSON.stringify(temporalJson);
+            fs.writeFileSync('lecturas.json', jsonText);
+            console.log('Array saved to Json file');
+            resolve();
+        } catch (error) {
+            reject(error);
+        }        
     })
 }
 
 
 // * EXPRESS ROUTES
 
-app.post('/temp', (req, res) => {
-    let data = req.body; // Almacenamos los datos del body del request en un variable
-    
-    // Iniciamos cadena de procesos en el orden deseado
-    loadJsonFileData()
+app.post('/temp/reg', (req, res) => {
+
+    // Hacemos una pequeña validación para asegurarnos de que el servidor recibe un Json, antes de iniciar el proceso de registro. Si no recibe un Json, lo indica por consola y manda mensaje a la API.
+    if (req.headers['content-type'] === 'application/json') {
+
+        const data = req.body; // Almacenamos los datos del body del request en un variable
+
+        // Iniciamos cadena de procesos en el orden deseado
+        loadJsonFileData()
         .then(addSentData(data))
         .then(registerJsonToFile())
         .then(res.send('Data recieved and registered'))
-        .catch(error => console.log(error));    
+        .catch( error => {
+            console.log('There was an error at some point', error);
+            res.send('There was an error at some point', error);
+        });
+
+    } else {
+        console.log('Recieved request is not a Json object');
+        res.send('Recieved request is no a Json object');
+    }    
 });
 
 
